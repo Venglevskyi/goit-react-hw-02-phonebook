c3f936baa36d94e90407ccf5023e88c220543575
Roman <Roman.Vengl1@gmail.com>
2020-01-29 17:48:01 +0200

    Updates

---
+++ precache-manifest.d0f15f264681dba6e997c319c21a5fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47921654160ce3839a4313438050d43b",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "829c07257e8207708eb3",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.719bec1b.chunk.css"
+  },
+  {
+    "revision": "e9b23e8caf2433915553",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.942aa979.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.942aa979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "829c07257e8207708eb3",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.4a8a64dc.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
